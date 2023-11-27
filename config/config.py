b72e9a93537d76902c3921ae3407db346d1bf12d
Karim Abdellatif <karim.abdellatif@enmacc.com>
2023-11-28 00:03:13 +0100

    Remove config files as Config is coming from git.

--- config/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    environment_name: str
-    account_id: str
-    region: str
-
--- config/dev.config.py
+++
@@ -1,7 +0,0 @@
-from config import Config
-
-
-class DevConfig(Config):
-    environment_name = "dev"
-    account_id = "907833391197"
-    region = "eu-central-1"
